43a632c0c57e7d2fa58a23e68b5d28a3218bf919
Drew Neil <andrew.jr.neil@gmail.com>
2010-10-19 12:26:56 +0100

    Insert newline between each template.

--- application.rb
+++ application.rb
@@ -7,7 +7,7 @@ helpers do
       name = File.basename(template, ".html")
       content = open(template).read
       "<textarea id='#{name}' class='x-hidden-display'>#{content}</textarea>"
-    end
+    end.join("\n")
   end
 end
 
