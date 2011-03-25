28d2b0a3dcf72f864c3f4b90a8c2243052a9a539
Drew Neil <andrew.jr.neil@gmail.com>
2011-03-25 16:50:59 +0200

    Dynamically insert <script> tags for each .js file in app/**

--- application.rb
+++ application.rb
@@ -9,6 +9,20 @@ helpers do
       "<textarea id='#{name}' class='x-hidden-display'>#{content}</textarea>"
     end.join("\n")
   end
+
+  def link_scripts
+    sources = ["public/lib/sencha-touch.js"]
+    sources += Dir.glob("public/app/*.js")
+    %w{models stores views controllers}.each do |layer|
+      sources += Dir.glob("public/app/#{layer}/**")
+    end
+
+    sources.map do |file|
+      source = file.sub('public/', '')
+      "<script src='#{source}' type='text/javascript'></script>"
+    end.join("\n  ")
+  end
+
 end
 
 get '/' do
